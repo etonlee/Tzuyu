2696894d82e060340c2dcaab6fa7b2713d4f78da
etonlee <51206420+etonlee@users.noreply.github.com>
2019-06-03 10:34:50 -0400

    Hi
    PLEASE WORK

--- HelloWorld/src/HelloTzuyu.java
+++ HelloWorld/src/HelloTzuyu.java
@@ -1,9 +1,10 @@
 
-public class HelloTzuyu {
+public class HelloTzuyu 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello Tzuyu");
 	}
 
 }
